1fc29bedf02d5bf5ba683c8ef171ab808431ea89
RazorSh4rk <iamrazorshark@gmail.com>
2020-01-10 00:01:44 +0100

    added option to remove swear words

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ if(db.all() == []):
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 
-words_file = open('words.json', 'r')
-words_text = words_file.read()
-words_json = json.loads(words_text)
+words_json = json.loads( open('words.json', 'r').read() )
+swear_json = json.loads( open('swear.json', 'r').read() )
 
 @app.route('/')
 def _index():
@@ -29,15 +28,28 @@ def _key():
 @cross_origin()
 def _word():
     key = request.args.get('key')
+
+    #filter swear words, default to off
+    try:
+        swear = int(request.args.get('swear'))
+    except:
+        swear = 0
+    if swear == 0:
+        dictionary = words_json
+    if swear == 1:
+        dictionary = words_json + swear_json
+        
     try:
         number = int(request.args.get('number'))
     except:
         number = 1
+        
     rec = Query()
     if(db.search(rec.key == key) != []):
         all_words = []
+            
         for i in range(0, number):
-            word = words_json[randint(0, len(words_json) - 1)]
+            word = dictionary[randint(0, len(dictionary) - 1)]
             all_words.append(word)
         return Response(json.dumps(all_words), mimetype='application/json')
         
@@ -47,9 +59,21 @@ def _word():
 @cross_origin()
 def _all():
     key = request.args.get('key')
+
+    #filter swear words, default to off
+    try:
+        swear = int(request.args.get('swear'))
+    except:
+        swear = 0
+        
+    if swear == 0:
+        dictionary = words_json
+    if swear == 1:
+        dictionary = words_json + swear_json
+        
     rec = Query()
     if(db.search(rec.key == key) != []):
-        return Response(json.dumps(words_json), mimetype='application/json')
+        return Response(json.dumps(dictionary), mimetype='application/json')
         
     return Response('"wrong API key"', mimetype='application/json')
 
