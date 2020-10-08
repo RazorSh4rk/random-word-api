5e346f8e989a0316b7052dbfaf3b66a62ee5b5d6
RazorSh4rk <iamrazorshark@gmail.com>
2020-10-09 01:26:22 +0200

    added a rate limit with redis

--- build.sbt
+++ build.sbt
@@ -7,4 +7,5 @@ version := "1.0"
 enablePlugins(JavaAppPackaging)
 
 libraryDependencies ++= Seq("com.typesafe.play" %% "play-json" % "2.8.1",
-"com.lihaoyi" %% "cask" % "0.5.6")
+"com.lihaoyi" %% "cask" % "0.5.6",
+"net.debasishg" %% "redisclient" % "3.30")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,7 @@ import scala.io.Source
 import scala.util.Random
 import scala.util.Properties
 import java.net.URI
+import com.redis._
 
 object Main extends cask.MainRoutes {
   override val port: Int = 
@@ -11,6 +12,10 @@ object Main extends cask.MainRoutes {
 
   override val host: String = 
     Properties.envOrElse("HOST", "0.0.0.0")
+
+  val redisUrl = 
+    URI.create(Properties.envOrElse("REDIS_URL", "http://localhost:6379"))
+  val redisConnection = new RedisClient(redisUrl)
  
   val words = Json.parse(Source.fromFile("words.json").getLines().mkString)
   val swearWords = Json.parse(Source.fromFile("swear.json").getLines().mkString)
@@ -18,6 +23,17 @@ object Main extends cask.MainRoutes {
     ("content-type", "application/json"),
     ("Access-Control-Allow-Origin", "*")
   )
+
+  val checkRateLimit = (IP: String) => {
+  	redisConnection.get(IP) match {
+  		case None => {
+  				redisConnection.set(IP, 1)
+  				redisConnection.expire(IP, 5)
+  				true
+  			}
+  		case Some(x) => false
+  	}
+  }
 
   @cask.get("/")
   def redir() = {
@@ -30,22 +46,54 @@ object Main extends cask.MainRoutes {
   }
 
   @cask.get("/all")
-  def getAll(swear: Int = 0) = {
-    if(swear == 0) 
-      cask.Response(Json.stringify(words), headers = headers)
-    else 
-      cask.Response(Json.stringify( Json.toJson( words.as[List[String]]:::(swearWords.as[List[String]]) ) ) , headers = headers)
+  def getAll(req: cask.Request, swear: Int = 0) = {
+  	val IP = req.exchange.getSourceAddress.toString
+  	if(checkRateLimit(IP)){
+	    if(swear == 0) 
+	      cask.Response(Json.stringify(words), headers = headers)
+	    else 
+	      cask.Response(Json.stringify( Json.toJson( words.as[List[String]]:::(swearWords.as[List[String]]) ) ) , headers = headers)
+    } else cask.Response(
+       		Json.stringify(Json.toJson(Map("Error" -> "You hit the rate limit, try again in a few seconds"))),
+        	statusCode = 403,
+       		headers = headers
+    	)
   }
 
   @cask.get("/word")
-  def getWord(number: Int = 1, swear: Int = 0) = {
-    val r = new Random
-    var w = words.as[List[String]]
-    if(swear == 1)
-      w = w:::swearWords.as[List[String]]
-    val ret = Random.shuffle(w).take(number)
+  def getWord(req: cask.Request, number: Int = 1, swear: Int = 0) = {
+  	val IP = req.exchange.getSourceAddress.toString
+  	if(checkRateLimit(IP)){
+	    val r = new Random
+	    var w = words.as[List[String]]
+	    if(swear == 1)
+	      w = w:::swearWords.as[List[String]]
+	    val ret = Random.shuffle(w).take(number)
+	
+	    cask.Response(Json.stringify(Json.toJson(ret)), headers = headers)
+    } else cask.Response(
+    		Json.stringify(Json.toJson(Map("Error" -> "You hit the rate limit, try again in a few seconds"))),
+    		statusCode = 403,
+    		headers = headers
+    	)
+  }
 
-    cask.Response(Json.stringify(Json.toJson(ret)), headers = headers)
+  // endpoint to check redis entries because i cant do that in heroku
+  @cask.get("/redis_dump")
+  def redisDump(pass: String) = {
+  	val _pass = scala.util.Properties.envOrElse(pass, "")
+  	if(pass != "") {
+  		val keys: List[String] = redisConnection.keys("*") match {
+  			case None => List()
+  			case Some(x) => {
+  				x.map(el => el match {
+  					case None => ""
+  					case Some(x) => x.toString
+  				})
+  			}
+  		}
+  		keys.mkString("\n")
+  	} else ""
   }
 
   initialize()
