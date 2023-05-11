74f103e5c53ed7e11ea1367a2ee125bdc744f9f5
RazorSh4rk <iamrazorshark@gmail.com>
2023-05-12 00:48:25 +0200

    update docs site

--- templates/script.js
+++ templates/script.js
@@ -1,29 +1,55 @@
-const section_names = [
-    "endpoints",
-    "parameters",
-    "license",
-    "donate"
-]
+var converter = new showdown.Converter();
+var markdownText = `# Random word API
 
-const sections = section_names.map(el => document.getElementById(el))
-const buttons = section_names.map(el => document.getElementById(el + "_btn"))
+## Endpoints
 
-buttons.forEach(el => el.addEventListener("click", evt => {
-    const id = evt.target.id.replace("_btn", "")
-    sections.forEach(el => {
-        if(el.id == id) el.style.display = "block"
-        else el.style.display = "none"
-    })
-}))
+### /all
+Return all stored words. This will be a huge data dump and will take a long time to fetch and will probably slow down your application.
 
-let copy = (text) => navigator.clipboard.writeText(text)
+    Example:
+    https://random-word-api.herokuapp.com/all
 
-// show endpoints by default
-sections.forEach(el => {
-    if(el.id == "endpoints") el.style.display = "block"
-    else el.style.display = "none"
-})
+---
 
-fetch("https://random-word-api.herokuapp.com/languages")
-    .then(r => r.json())
-    .then(r => document.getElementById("langs").textContent = r)
+### /word
+Return one stored word randomly. Options can be further specified, see the parameters menu.
+
+    Example:
+    https://random-word-api.herokuapp.com/word
+
+---
+
+### /languages
+Return a list of stored language codes. Useful if you want a multilanguage app. See Github to find out how to add your own. 
+
+    Example:
+    https://random-word-api.herokuapp.com/languages
+
+## Parameters
+
+### ?number
+    
+Set number of requested words. If it exceeds the maximum stored amount, it will just return all of them.
+
+    Example:
+    https://random-word-api.herokuapp.com/word?number=10
+
+### ?length
+    
+Set length of requested words. This will only return words that contain x amount of letters.
+
+    Example:
+    https://random-word-api.herokuapp.com/word?length=5
+
+### ?lang
+    
+Set language of requested words. Go to Github to get more info on how to add your own language. 
+
+    Currently supported languages:
+    https://random-word-api.herokuapp.com/languages
+    Example:
+    https://random-word-api.herokuapp.com/word?lang=es
+
+`
+var html = converter.makeHtml(markdownText);
+document.getElementById('markdown').innerHTML = html;
